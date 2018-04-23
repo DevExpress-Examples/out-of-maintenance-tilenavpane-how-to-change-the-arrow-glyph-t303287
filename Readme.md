6abd642c647c6ec62501811316bafd95ec27610a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# TileNavPane - How to change the arrow glyph
+
+
+This example illustrates how to change TileNavPane's arrow (separator) glyph<br /><br /><img src="https://raw.githubusercontent.com/DevExpress-Examples/tilenavpane-how-to-change-the-arrow-glyph-t303287/14.2.3+/media/fc436907-78bb-11e5-80bf-00155d62480c.png">
+
+<br/>
+
+
---
+++ VB/WindowsFormsApplication79/CustomTileNavPane.vb
@@ -0,0 +1,29 @@
+﻿Imports DevExpress.XtraBars.Navigation
+Imports System
+Imports System.Collections.Generic
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+
+Namespace WindowsFormsApplication79
+    Public Class CustomTileNavPane
+        Inherits TileNavPane
+
+        Private _arrowGlyph As Bitmap
+        Public Sub New()
+            MyBase.New()
+
+        End Sub
+        Protected Overrides Function CreateViewInfo() As TileNavPaneViewInfo
+            Return New CustomTileNavPaneViewInfo(Me)
+        End Function
+        Public Property ArrowGlyph() As Bitmap
+            Get
+                Return _arrowGlyph
+            End Get
+            Set(ByVal value As Bitmap)
+                _arrowGlyph = value
+            End Set
+        End Property
+    End Class
+End Namespace
